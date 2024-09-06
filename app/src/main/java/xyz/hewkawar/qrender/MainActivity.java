3f4cbcd49caf5dcf8d42dead96593f317e01b7ce
HewkawAr <hewkawar@gmail.com>
2024-09-06 19:28:27 +0700

    Base Code

--- app/src/main/java/xyz/hewkawar/qrender/MainActivity.java
+++ app/src/main/java/xyz/hewkawar/qrender/MainActivity.java
@@ -1,6 +1,12 @@
 package xyz.hewkawar.qrender;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +14,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+    public IntentIntegrator integrator;
+
+    public TextView debugText;
+
+    public void openExternalBrowser(String url) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+
+        startActivity(browserIntent);
+    }
+
+    public boolean isValidURL(String urlString) {
+        try {
+            new URL(urlString);
+            return true;
+        } catch (MalformedURLException e) {
+            return false;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +50,44 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        integrator = new IntentIntegrator(this);
+        integrator.setOrientationLocked(true);
+
+        Button openScannerBtn = findViewById(R.id.openScanner);
+        debugText = findViewById(R.id.debugText);
+
+        openScannerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                integrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, getString(R.string.cancelled), Toast.LENGTH_SHORT).show();
+            } else {
+                String text = result.getContents();
+                if (text.startsWith("WIFI:")) {
+                    Toast.makeText(this, getString(R.string.not_support_wifi_format), Toast.LENGTH_SHORT).show();
+                } else if (isValidURL(text)) {
+                    Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+                    debugText.setText(text);
+                    openExternalBrowser(result.getContents());
+                } else {
+                    Toast.makeText(this, getString(R.string.not_support_code), Toast.LENGTH_SHORT).show();
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
