f83355b6dc94614a9a8f4081397acb03ccfee44d
HewkawAr <hewkawar@gmail.com>
2024-10-04 15:18:35 +0700

    Fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "xyz.hewkawar.qrender"
-    compileSdk = 30
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "xyz.hewkawar.qrender"
--- app/src/main/java/xyz/hewkawar/qrender/MainActivity.java
+++ app/src/main/java/xyz/hewkawar/qrender/MainActivity.java
@@ -9,7 +9,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -33,31 +32,20 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-    public TextView debugText;
-    public Button copyTextBtn;
-
     private ActivityResultLauncher<Intent> scanQrResultLauncher;
 
     private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is not in the Support Library.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "Result";
-            String description = "Scan result";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("RESULT", name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this.
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
+        CharSequence name = "Result";
+        String description = "Scan result";
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel("RESULT", name, importance);
+        channel.setDescription(description);
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
     }
-
 
     public void openExternalBrowser(String url) {
         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-
         startActivity(browserIntent);
     }
 
@@ -68,13 +56,6 @@ public class MainActivity extends AppCompatActivity {
         } catch (MalformedURLException e) {
             return false;
         }
-    }
-
-    private void copyToClipboard(String text) {
-        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-        ClipData clip = ClipData.newPlainText("QR Scan Result", text);
-        clipboard.setPrimaryClip(clip);
-        Toast.makeText(this, "Copied to clipboard", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -98,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
 
         scanQrResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
-                resultData ->{
+                resultData -> {
                     if (resultData.getResultCode() == RESULT_OK) {
                         ScanIntentResult result = ScanIntentResult.parseActivityResult(resultData.getResultCode(), resultData.getData());
 
@@ -107,10 +88,7 @@ public class MainActivity extends AppCompatActivity {
                         } else {
                             String text = result.getContents();
 
-                            debugText.setText(text);
-
                             if (isValidURL(text)) {
-                                Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
                                 openExternalBrowser(text);
                             } else {
                                 NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "RESULT")
@@ -123,16 +101,13 @@ public class MainActivity extends AppCompatActivity {
                                                 PendingIntent.getBroadcast(this, 0,
                                                         new Intent(this, CopyBroadcastReceiver.class)
                                                                 .putExtra("textToCopy", text),
-                                                        PendingIntent.FLAG_UPDATE_CURRENT)).build());
+                                                        PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE)).build());
 
                                 NotificationManager notificationManager = getSystemService(NotificationManager.class);
                                 notificationManager.notify(1, builder.build());
                             }
 
-                            debugText.setVisibility(View.VISIBLE);
-                            copyTextBtn.setVisibility(View.VISIBLE);
-
-                            scanQrResultLauncher.launch(new ScanContract().createIntent(MainActivity.this, scanOptions));
+                            this.finishAffinity();
                         }
                     }
                 });
